014c05aab378f25c7df3488e5c5cea0aab39d3b4
eamazigo <94367281+eamazigo@users.noreply.github.com>
2021-12-20 22:03:59 -0500

    Update BicycleDAOtest.java

--- miniProjectOne/src/test/java/miniProjectOne/BicycleDAOtest.java
+++ miniProjectOne/src/test/java/miniProjectOne/BicycleDAOtest.java
@@ -1,25 +1,51 @@
 package miniProjectOne;
 import projectZero.Bicycle;
+import projectZero.BicycleDAO;
+import projectZero.BicyclePostgres;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertNotEquals;
+import java.util.Set;
 
 public class BicycleDAOtest {
-@Test
+	
+	private BicycleDAO bicycleDao = new BicyclePostgres();
+	
+	@Test 
+	public void getByIdWhenIdExists() {
+		// test setup
+		int idInput = 1;
+		// call the method to be tested
+		Bicycle idOutput = bicycleDao.getById(idInput);
+		// assert that it did what we expected
+		assertEquals(1, idOutput.getId());
+	}
+	
+
+	@Test
+	public void getByIdWhenIdDoesNotExists() {
+		int idInput = -1;
+		Bicycle petOutput =bicycleDao.getById(idInput);
+		assertNull(petOutput);
+	}
+	
+
+	@Test
+	public void getAll() {
+		Set<Bicycle> givenOutput = bicycleDao.getAll();
+		assertNull(givenOutput);
+		// assertNotNull(bikeDao.getall();
+	}
+	
+	@Test
 	public void addNewBicycle() {
 		Bicycle newBicycle = new Bicycle();
 		System.out.println(newBicycle);
 		
-		int generatedBikeId = BicycleDAOtest.create(newBicycle);
+		int generatedId = bicycleDao.create(newBicycle);
 		
-		assertNotEquals(0, generatedBikeId);
+		assertNotEquals(0, generatedId);
+		System.out.println(generatedId);
 	}
-
-private static int create(Bicycle newBicycle) {
-	// TODO Auto-generated method stub
-	return 0;
+	
 }
-}
-
-
-
